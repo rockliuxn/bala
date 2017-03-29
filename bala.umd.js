b105bfb7efc447ac04682e81c79edea1d5785214
Rock <rock.liuxn@gmail.com>
2017-03-29 17:35:12 +0800

    add branch es5
    1、balajs add e5 versions

--- bala.umd.js
+++ bala.umd.js
@@ -1,32 +1,41 @@
 (function(root, $) {
-	$ = ((document, s_querySelectorAll, fn, $) => (
-	($ = (s, context, bala) => (
-		(bala = Object.create(fn)),
+	$ = (function(document, s_addEventListener, s_querySelectorAll) {
+	function $(s, context, bala) {
+		bala = Object.create($.fn);
 
-		s && bala.push( // if s is truly then push the following
-			...(s.dispatchEvent // if arg is node or window,
+		s && bala.push.apply(bala, // if s is truly then push the following
+			s[s_addEventListener] // if arg is node or window,
 				? [s] // then pass [s]
-				: typeof s == 'string' // else if arg is a string
+				: "" + s === s // else if arg is a string
 					? /</.test(s) // if the string contains "<" (if HTML code is passed)
 						// then parse it and return node.children
-						// use 'querySelectorAll' (HTMLUnknownElement) if content is not presented
-						? ((context = document.createElement(context || s_querySelectorAll)).innerHTML = s
+						// use 'addEventListener' (HTMLUnknownElement) if content is not presented
+						? ((context = document.createElement(context || s_addEventListener)).innerHTML = s
 							, context.children)
 						: context // else if context is truly
 							? ((context = $(context)[0]) // if context element is found
 								? context[s_querySelectorAll](s) // then select element from context
 								: bala) // else pass [] (context isn't found)
 							: document[s_querySelectorAll](s) // else select elements globally
-					: s.length ? s : fn)), // else guessing that s variable is array-like Object
-        bala
-	)),
+					: typeof s == 'function' // else if function is passed
+						// if DOM is ready
+						// readyState[7] means readyState value is "interactive" or "complete" (not "loading")
+						? document.readyState[7]
+							? s() // then run given function
+							: document[s_addEventListener]('DOMContentLoaded', s) // else wait for DOM ready
+						: s); // else guessing that s variable is array-like Object
 
-	($.fn = fn),
+		return bala;
+	}
 
-	($.one = (s, context) => $(s, context)[0]),
+	$.fn = [];
 
-	$
-))(document, 'querySelectorAll', []);
+	$.one = function(s, context) {
+		return $(s, context)[0] || null;
+	};
+
+	return $;
+})(document, 'addEventListener', 'querySelectorAll');
 ;
 
 
